12938bebee86bd9c6ad7041d931a94a53df9127a
anupbhat30 <anupbhat67@gmail.com>
2023-10-03 01:24:44 +0530

    second

--- lib/cpage.dart
+++ lib/cpage.dart
@@ -19,38 +19,55 @@ class _CPageState extends State<CPage> {
     UserPage(),
   ];
 
+  bool showAppBar = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF907F9F),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Row(
-          children: [
-            SizedBox(width: 525,),
-            Image.asset('assets/temp/c.png', height: 50, width: 50),
-            Text(
-              'C',
-              style: TextStyle(
-                color: Color(0xFF907F9F),
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
+      appBar: showAppBar
+          ? AppBar(
+              backgroundColor: Colors.white,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 10), // Add some spacing to the left
+                  Image.asset(
+                    'assets/temp/c.png',
+                    height: 40, // Adjust the height as needed
+                    width: 40, // Adjust the width as needed
+                  ),
+                  Text(
+                    'C',
+                    style: TextStyle(
+                      color: Color(0xFF907F9F),
+                      fontSize: 30, // Adjust the font size as needed
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-      ),
+            )
+          : null,
       body: pages[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
-              icon: Icon(Icons.home), label: 'Home'),
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.filter_vintage_rounded), label: 'Biomimicry'),
+            icon: Icon(Icons.filter_vintage_rounded),
+            label: 'Biomimicry',
+          ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.leaderboard_rounded), label: 'Leaderboard'),
+            icon: Icon(Icons.leaderboard_rounded),
+            label: 'Leaderboard',
+          ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.supervised_user_circle_rounded), label: 'User'),
+            icon: Icon(Icons.supervised_user_circle_rounded),
+            label: 'User',
+          ),
         ],
         selectedItemColor: Color(0xFF907F9F),
         unselectedItemColor: Colors.grey,
@@ -60,6 +77,9 @@ class _CPageState extends State<CPage> {
         onTap: (int index) {
           setState(() {
             currentIndex = index;
+
+            // Hide the app bar when navigating to UserPage (index 3)
+            showAppBar = index != 3;
           });
         },
       ),
--- lib/userpage.dart
+++ lib/userpage.dart
@@ -1,10 +1,243 @@
 import 'package:flutter/material.dart';
-class UserPage extends StatelessWidget{
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
+
+class UserPage extends StatelessWidget {
+  void _editProfile() {
+    // Add functionality for Edit Profile button here
+  }
+
+  void _openAppearance() {
+    // Add functionality for Appearance button here
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF907F9F),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'User Profile',
+          style: TextStyle(
+            color: Color(0xFF907F9F),
+            fontSize: 45,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            Center(
+              child: CircularProfileAvatar(
+                '',
+                child: Image.asset(
+                  'assets/temp/user.png',
+                  fit: BoxFit.cover,
+                ),
+                borderColor: Colors.white,
+                borderWidth: 3,
+                radius: 75,
+              ),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Text(
+                'Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Text(
+                'Bio',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: _editProfile,
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF907F9F),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Text(
+                      'Edit Profile',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: _openAppearance,
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF907F9F),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Text(
+                      'Appearance',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Statistics',
+                    style: TextStyle(
+                      color: Color(0xFF907F9F),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset('assets/temp/fire.png',
+                                height: 18, width: 18),
+                            SizedBox(width: 10),
+                            Text(
+                              'Streaks',
+                              style: TextStyle(
+                                color: Color(0xFF907F9F),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Image.asset('assets/temp/leaderboard.png',
+                                height: 40, width: 40),
+                            SizedBox(width: 10),
+                            Text(
+                              'Leaderboard',
+                              style: TextStyle(
+                                color: Color(0xFF907F9F),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset('assets/temp/clock.png',
+                                height: 18, width: 18),
+                            SizedBox(width: 10),
+                            Text(
+                              'Time Spent',
+                              style: TextStyle(
+                                color: Color(0xFF907F9F),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Image.asset('assets/temp/book.png',
+                                height: 25, width: 25),
+                            SizedBox(width: 10),
+                            Text(
+                              'Courses',
+                              style: TextStyle(
+                                color: Color(0xFF907F9F),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: Text(
+                'Log Out',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
-
 }
