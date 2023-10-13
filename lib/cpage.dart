f4a6c025c3e0667120add50b65b16733bee51a4e
anupbhat30 <anupbhat67@gmail.com>
2023-10-13 22:24:37 +0530

    second

--- lib/cpage.dart
+++ lib/cpage.dart
@@ -18,37 +18,11 @@ class _CPageState extends State<CPage> {
     LeaderboardPage(),
     UserPage(),
   ];
-
   bool showAppBar = true;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF907F9F),
-      appBar: showAppBar
-          ? AppBar(
-              backgroundColor: Colors.white,
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(width: 10), // Add some spacing to the left
-                  Image.asset(
-                    'assets/temp/c.png',
-                    height: 40, // Adjust the height as needed
-                    width: 40, // Adjust the width as needed
-                  ),
-                  Text(
-                    'C',
-                    style: TextStyle(
-                      color: Color(0xFF907F9F),
-                      fontSize: 30, // Adjust the font size as needed
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            )
-          : null,
       body: pages[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: [
@@ -77,8 +51,7 @@ class _CPageState extends State<CPage> {
         onTap: (int index) {
           setState(() {
             currentIndex = index;
-
-            // Hide the app bar when navigating to UserPage (index 3)
+            showAppBar = index != 2;
             showAppBar = index != 3;
           });
         },
--- lib/leaderboard.dart
+++ lib/leaderboard.dart
@@ -1,11 +1,132 @@
 import 'package:flutter/material.dart';
-class LeaderboardPage extends StatelessWidget{
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-       backgroundColor: Color(0xFF907F9F),
-     );
+    return MaterialApp(
+      title: 'Leaderboard',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LeaderboardPage(),
+    );
+  }
+}
 
-      }
+class LeaderboardPage extends StatelessWidget {
+  final List<User> users = [
+    User(
+        name: 'User 1',
+        avatarUrl: 'https://example.com/avatar1.jpg',
+        score: 1000),
+    User(
+        name: 'User 2',
+        avatarUrl: 'https://example.com/avatar2.jpg',
+        score: 900),
+    User(
+        name: 'User 3',
+        avatarUrl: 'https://example.com/avatar3.jpg',
+        score: 800),
+    User(
+        name: 'User 4',
+        avatarUrl: 'https://example.com/avatar4.jpg',
+        score: 700),
+    User(
+        name: 'User 5',
+        avatarUrl: 'https://example.com/avatar5.jpg',
+        score: 600),
+  ];
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Leaderboard',
+              style: TextStyle(
+                color: Color(0xFF907F9F),
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+      ),
+      backgroundColor: Color(0xFF907F9F),
+      body: ListView(
+        children: [
+          buildTopThreeAvatars(),
+          Divider(),
+          buildUserList(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildTopThreeAvatars() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        buildPodiumUser(users[1], 2), // Middle avatar with rank 1
+        buildPodiumUser(users[0], 1), // First avatar
+        buildPodiumUser(users[2], 3), // Third avatar
+      ],
+    );
+  }
+
+  Widget buildPodiumUser(User user, int rank) {
+    return Column(
+      children: [
+        SizedBox(height: rank == 1 ? 10.0 : 0.0),
+        CircleAvatar(
+          backgroundImage: NetworkImage(user.avatarUrl),
+          radius: 48.0,
+        ),
+        SizedBox(height: 8.0),
+        Text(
+          'Rank $rank',
+          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          user.name,
+          style: TextStyle(fontSize: 18.0),
+        ),
+        Text(
+          '${user.score} Points',
+          style: TextStyle(fontSize: 18.0),
+        ),
+      ],
+    );
+  }
+
+  Widget buildUserList() {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        final user = users[index];
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(user.avatarUrl),
+          ),
+          title: Text(user.name),
+          trailing: Text('${user.score} Points'),
+        );
+      },
+    );
+  }
+}
+
+class User {
+  final String name;
+  final String avatarUrl;
+  final int score;
+
+  User({
+    required this.name,
+    required this.avatarUrl,
+    required this.score,
+  });
 }
