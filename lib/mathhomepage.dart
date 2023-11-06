8cdb99988e6242bcbda92eb33508f8e43c92d0ee
Hamdan Shaik <hamdanshaikh11133@gmail.com>
2023-11-07 01:48:46 +0530

    4th

--- lib/mathhomepage.dart
+++ lib/mathhomepage.dart
@@ -1,42 +1,99 @@
 import 'package:flutter/material.dart';
-
 class MathHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        floatingActionButton: Container(
-          height: 50,
-          width: 50,
-          child: FloatingActionButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/c');
-              },
-              child: Image.asset('assets/temp/c.png', height: 30, width: 30)),
+      backgroundColor: Color(0xFF907F9F),
+      floatingActionButton: Container(
+        height: 50,
+        width: 50,
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/c');
+          },
+          child: Image.asset('assets/temp/c.png', height: 30, width: 30),
         ),
-        backgroundColor: Color(0xFF907F9F),
-        appBar: AppBar(
-          leading:null,
-          backgroundColor: Colors.white,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(width: 10),
-              Image.asset(
-                'assets/temp/math.png',
-                height: 40,
-                width: 40,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/temp/mnemomap.png'),
+                fit: BoxFit.cover,
               ),
-              Text(
-                'Math',
-                style: TextStyle(
-                  color: Color(0xFF907F9F),
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
+            ),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
           ),
-        ));
+          Positioned(
+            top: 120.0,
+            left: 230.0,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/mlv1');
+                          },
+                        child: Image.asset('assets/temp/level.png', height:45, width:45)
+                    ),
+                Positioned(
+                  top: 10,
+                  child: Text(
+                    '1', style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                ),
+                  ],
+            ),
+            ),
+          Positioned(
+            top: 235.0,
+            left: 230.0,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                    GestureDetector(
+                onTap: () {
+                      Navigator.pushNamed(context, '/');},
+                    child: Image.asset('assets/temp/level.png', height:45, width:45)
+                    ),
+                Text(
+                    '2', style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+                ),
+                  ],
+            ),
+          ),
+          Positioned(
+            top: 370.0,
+            left: 230.0,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                    GestureDetector(
+                onTap: () {
+                      Navigator.pushNamed(context, '/');},
+                    child: Image.asset('assets/temp/level.png', height:45, width:45)
+                    ),
+                Text('3',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+                )
+                  ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
